e23d0de5532b2bd5f2bddeacc833677b3cde16ea
Paulo Tada <paulohtfs@gmail.com>
2018-05-06 06:18:38 -0300

    Corrige rota de index para groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,14 @@
 class GroupsController < ApplicationController
 
-  before_action :set_group, only: [:show]
+  before_action :set_group, only: [:show, :update]
 
   def index
     @groups = User.first.groups
-    head :ok
+    render :index, status: :ok
   end
 
   def show
+    @itinerary = Itinerary.compile_itinerary(@group.id)
   end
 
   def create
@@ -15,11 +16,19 @@ class GroupsController < ApplicationController
     head :ok
   end
 
+  def update
+    if @group.update(group_params)
+      head :ok
+    else
+      head :bad_request
+    end
+  end
+
   private
 
   def set_group
     @group = User.first.groups.find(params[:id])
-    head :not_fount unless @group
+    head :not_found unless @group
   end
 
   def group_params
